161e536b8e9ff7335b74c750bffd4c50bc3c3e5c
BHARGAVA2002 <95297113+BHARGAVA2002@users.noreply.github.com>
2023-06-18 22:29:43 +0530

    test.py

--- test.py
+++ test.py
@@ -1,7 +1,1 @@
-from skimage.filters import gaussian
-import interface
-from PIL import ImageFilter
-interface.start()
-img=interface.img
-temp=img.filter(ImageFilter.GaussianBlur(1))
-temp.show()
+
